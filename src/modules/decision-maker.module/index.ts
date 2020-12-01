764b22abd90553a4a2cf9c06b09b603231765334
Jake <ljakeail@gmail.com>
2020-12-01 17:05:22 -0500

    Update index.ts

--- src/modules/decision-maker.module/index.ts
+++ src/modules/decision-maker.module/index.ts
@@ -2,9 +2,10 @@ import { Module }                  from '@nestjs/common';
 import { DecisionMakerService }    from './service';
 import { DecisionMakerController } from './controller';
 import { TemperatureModule }       from '../temperature.module';
+import { WebThermostatModule }     from '../web-thermostat.module';
 
 @Module({
-  imports: [TemperatureModule],
+  imports: [TemperatureModule,WebThermostatModule],
   controllers: [DecisionMakerController],
   providers: [DecisionMakerService],
 })

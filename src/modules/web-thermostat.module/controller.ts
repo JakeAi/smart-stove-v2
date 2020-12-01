794052e73e2614fa2307b521682e25084575b19a
Jake <ljakeail@gmail.com>
2020-12-01 17:33:07 -0500

    Update controller.ts

--- src/modules/web-thermostat.module/controller.ts
+++ src/modules/web-thermostat.module/controller.ts
@@ -1,39 +1,43 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { HeatingCoolingState } from '../../constants';
-import { WebThermostatService } from './service';
-import { TemperatureService } from '../temperature.module/service';
+import { HeatingCoolingState }    from '../../constants';
+import { WebThermostatService }   from './service';
+import { TemperatureService }     from '../temperature.module/service';
 
 
-@Controller('web-thermostat')
+@Controller( 'web-thermostat' )
 export class WebThermostatController {
+  public currentTemperature = 0;
+
   constructor(
     private service: WebThermostatService,
     private temperatureService: TemperatureService,
-  ) { }
+  ) {
+    this.temperatureService.temperature.subscribe( temp => this.currentTemperature = temp );
+  }
 
-  @Get('status')
+  @Get( 'status' )
   status() {
     return {
-      targetHeatingCoolingState: this.service.targetHeatingCoolingState,
+      targetHeatingCoolingState : this.service.targetHeatingCoolingState,
       currentHeatingCoolingState: this.service.currentHeatingCoolingState,
-      targetTemperature: this.service.targetTemperature,
-      currentTemperature: this.service.currentTemperature,
+      targetTemperature         : this.service.targetTemperature,
+      currentTemperature        : this.service.currentTemperature,
     };
   }
 
-  @Get('/targetHeatingCoolingState')
-  targetHeatingCoolingState(@Query('value') targetHeatingCoolingState: string) {
-    let targetState: HeatingCoolingState = parseInt(targetHeatingCoolingState);
+  @Get( '/targetHeatingCoolingState' )
+  targetHeatingCoolingState( @Query( 'value' ) targetHeatingCoolingState: string ) {
+    let targetState: HeatingCoolingState = parseInt( targetHeatingCoolingState );
     this.service.targetHeatingCoolingState = targetState;
-    console.log({ targetState });
+    console.log( { targetState } );
   }
 
-  @Get('/targetTemperature')
-  targetTemperature(@Query('value') targetTemperature: string) {
-    let temp = Math.floor(parseFloat(targetTemperature));
-    let temperatureF = temp * (9 / 5) + 32.0;
+  @Get( '/targetTemperature' )
+  targetTemperature( @Query( 'value' ) targetTemperature: string ) {
+    let temp = Math.floor( parseFloat( targetTemperature ) );
+    let temperatureF = temp * ( 9 / 5 ) + 32.0;
     this.service.targetHeatingCoolingState = temperatureF;
-    console.log({ temperatureF });
+    console.log( { temperatureF } );
   }
 
 }

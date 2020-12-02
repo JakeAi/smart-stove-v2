35ef2f80043ca8d46476cf5c258ca8700c9bf41f
Jake <ljakeail@gmail.com>
2020-12-01 20:56:25 -0500

    Update controller.ts

--- src/modules/temperature.module/controller.ts
+++ src/modules/temperature.module/controller.ts
@@ -1,21 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
-import { TemperatureService } from './service';
-import { TemperatureDirection } from '../../constants';
+import { Temperature, TemperatureService } from './service';
 
 @Controller('temperature')
 export class TemperatureController {
-  public currentTemperature = 0;
-  public averageTemperature = { averageTemperature: 0, averageTemperaturePrevious: 0, previousTemperatures: [] };
-  public direction = { weight: 0, direction: TemperatureDirection.DOWN };
+  public temperature: Temperature;
 
   constructor(private readonly temperatureService: TemperatureService) {
-    this.temperatureService.temperature.subscribe((temp) => this.currentTemperature = temp);
-    this.temperatureService.averageTemperature.subscribe((temp) => this.averageTemperature = temp);
-    this.temperatureService.direction.subscribe((temp) => this.direction = temp);
+    this.temperatureService.temperature$.subscribe((temp) => this.temperature = temp);
   }
 
   @Get()
   getTemperature() {
-    return { temperature: this.currentTemperature };
+    return { temperature: this.temperature.temperatureCurrent };
   }
 }
